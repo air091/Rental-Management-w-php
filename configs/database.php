87b1409d93db6e8f830223c5a844ee1e10e38620
Ace <acesparkle243@gmail.com>
2026-02-02 15:15:52 +0800

    fixes

---
+++ configs/database.php
@@ -0,0 +1,18 @@
+<?php
+  $host = "localhost";
+  $username = "postgres";
+  $password = "root";
+  $database = "rental_db_auth";
+  $port = "5432";
+
+  $dataSourceName = "pgsql:host:$host;port=$port;dbname=$database";
+
+  try {
+    $pdo = new PDO($dataSourceName, $username, $password, [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+    echo("Database connected");
+  } catch(PDOException $e) {
+    die("Database connection failed: ". $e->getMessage());
+  }
--- controllers/auth/register-handler.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once "../auth/database.php";
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-}
